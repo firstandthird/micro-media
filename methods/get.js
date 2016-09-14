c4c48a896d8275e45f2bebf8ce6804ac68085fb0
Chris <orthagonal1@gmail.com>
2016-09-13 19:20:12 -0500

    use vision/handlebars,
    refactors yaml

--- methods/get.js
+++ methods/get.js
@@ -2,11 +2,9 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 module.exports = function(key, callback) {
-  const options = this.registrations['hapi-upload-s3'].options;
-  const s3Options = {};
-  s3Options.Bucket = options.s3Bucket;
-  s3Options.Key = key;
-  s3.getObject(s3Options, (err, data) => {
+  const options = this.settings.app.s3;
+  options.Key = key;
+  s3.getObject(options, (err, data) => {
     callback(err, err ? undefined : data.Body);
   });
 };
--- routes/test.js
+++ routes/test.js
@@ -2,20 +2,6 @@ module.exports.dummy = {
   path: '/test',
   method: 'GET',
   handler: (request, reply) => {
-    reply(`
-<html>
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.js"/> </script>
-    <style>
-    </style>
-  </head>
-  <body>
-    <form action="/api/media" class="dropzone dz-clickable">
-      <div class="dz-default dz-message">
-        Click here to test mediamanager's upload!
-      </div>
-    </form>
-  </body>
-</html>`);
+    reply.view('test', {});
   }
 };
