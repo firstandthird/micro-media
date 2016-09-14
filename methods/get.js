1788cc1bcd2d78e87753dc71aa3ffd94693870c3
Chris <orthagonal1@gmail.com>
2016-09-14 12:36:53 -0500

    conf uses aws env vars, lowercase key names
    --fixed methods.get to be shorter, clearer, and use lowercase key names
    from conf

--- methods/get.js
+++ methods/get.js
@@ -1,10 +1,13 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-const clone = require('lodash.clonedeep');
+const s3Interface = new AWS.S3({ apiVersion: '2006-03-01' });
+
 module.exports = function(key, callback) {
-  const options = clone(this.settings.app.s3);
-  options.Key = key;
-  s3.getObject(options, (err, data) => {
+  const s3Options = this.settings.app.s3;
+  const s3Params = {
+    Bucket: s3Options.bucket,
+    Key: key
+  };
+  s3Interface.getObject(s3Params, (err, data) => {
     callback(err, err ? undefined : data.Body);
   });
 };
