e0e0297ca9c5cb995de71ba7fd601eef2d62945c
Chris <orthagonal1@gmail.com>
2016-09-13 19:44:54 -0500

    use clone, fixes region to u

--- methods/get.js
+++ methods/get.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-
+const clone = require('lodash.clonedeep');
 module.exports = function(key, callback) {
-  const options = this.settings.app.s3;
+  const options = clone(this.settings.app.s3);
   options.Key = key;
   s3.getObject(options, (err, data) => {
     callback(err, err ? undefined : data.Body);
