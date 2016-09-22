d0a86c807bbd658d3cd625391d6d4b9be9f39b24
Chris <orthagonal1@gmail.com>
2016-09-22 14:36:19 -0500

    /api/media now uses .map to generate url list

--- methods/list.js
+++ methods/list.js
@@ -6,10 +6,8 @@ module.exports = function(callback) {
   const s3Options = {};
   s3Options.Bucket = options.s3Bucket;
   s3.listObjectsV2(s3Options, (err, data) => {
-    const imageUrlList = [];
-    data.Contents.forEach((imageOnS3) => {
-      imageUrlList.push(`${this.settings.app.s3Host}/${options.s3Bucket}/${imageOnS3.Key}`)
-    });
-    callback(err, err ? undefined : imageUrlList);
+    callback(err, err ? undefined : data.Contents.map((imageOnS3) => {
+      return `${this.settings.app.s3Host}/${options.s3Bucket}/${imageOnS3.Key}`;
+    }));
   });
 };
