4dd3f5cc21e1406b0ed9c0ab208e553c374b8279
Chris <orthagonal1@gmail.com>
2016-10-03 15:01:07 -0500

    response -> reply in upload route

--- routes/upload.js
+++ routes/upload.js
@@ -8,15 +8,15 @@ module.exports.upload = {
       parse: true
     }
   },
-  handler: (request, response) => {
+  handler: (request, reply) => {
     request.payload.file.path = request.payload.file.hapi.filename
     request.server.methods.upload(request.payload.file, (err, result) => {
       if (err) {
         request.server.log(err);
         // todo: smarter error code?
-        return response('Not Found').code(500);
+        return reply('Not Found').code(500);
       }
-      return response(result);
+      return reply(result);
     });
   }
 };
