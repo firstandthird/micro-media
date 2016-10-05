64511db133e6900301887fb1f037025a0fcecc4b
Chris <orthagonal1@gmail.com>
2016-10-05 16:36:01 -0500

    fixed list.js to work with new config options

--- methods/list.js
+++ methods/list.js
@@ -2,12 +2,12 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 module.exports = function(callback) {
-  const options = this.registrations['hapi-upload-s3'].options;
-  const s3Options = {};
-  s3Options.Bucket = options.s3Bucket;
-  s3.listObjectsV2(s3Options, (err, data) => {
+  const options = {
+    Bucket: this.settings.app.s3.bucket
+  };
+  s3.listObjectsV2(options, (err, data) => {
     callback(err, err ? undefined : data.Contents.map((imageOnS3) => {
-      return `${this.settings.app.s3Host}/${options.s3Bucket}/${imageOnS3.Key}`;
+      return `${this.settings.app.s3Host}/${ options.Bucket }/${imageOnS3.Key}`;
     }));
   });
 };
