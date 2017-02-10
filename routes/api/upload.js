be15fa8cc7c7ccf726a5929233681d9e4a5c7b43
Greg Allen <greg@firstandthird.com>
2017-02-10 14:10:00 -0800

    refactor

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,18 +1,60 @@
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const fs = require('fs');
+
 exports.upload = {
   method: 'POST',
   path: 'upload',
   config: {
     payload: {
-      output: 'stream'
+      output: 'file',
+      parse: true
     }
   },
-  handler(request, reply) {
-    const server = request.server;
-    const options = {
-      folder: request.query.folder
-    };
-    const file = request.payload.file;
-    file.path = file.hapi.filename;
-    server.uploadToS3(file, options, reply);
+  handler: {
+    autoInject: {
+      payload(request, done) {
+        done(null, request.payload);
+      },
+      filepath(payload, done) {
+        done(null, payload.file.path);
+      },
+      filename(payload, done) {
+        done(null, payload.file.filename);
+      },
+      quality(request, done) {
+        const quality = request.query.quality || 80;
+        done(null, quality);
+      },
+      minBuffer(filepath, quality, done) {
+        imagemin([filepath], {
+          plugins: [
+            imageminMozjpeg({ quality }),
+            imageminPngquant({ quality })
+          ]
+        }).then(out => {
+          done(null, out[0].data);
+        }, (err) => {
+          done(err);
+        });
+      },
+      s3Options(request, filename, done) {
+        done(null, {
+          folder: request.query.folder,
+          public: (request.query.public),
+          path: filename
+        });
+      },
+      s3(server, minBuffer, filename, s3Options, done) {
+        server.uploadToS3(minBuffer, s3Options, done);
+      },
+      clean(minBuffer, filepath, done) {
+        fs.unlink(filepath, done);
+      },
+      reply(s3, done) {
+        done(null, s3);
+      }
+    }
   }
 };
