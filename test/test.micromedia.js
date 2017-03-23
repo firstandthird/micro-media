8a43f510be83397500e65ed2ddced4e74530e865
orthagonal <orthagonal1@gmail.com>
2017-03-22 22:12:29 -0500

    first checkin

---
+++ test/test.micromedia.js
@@ -0,0 +1,31 @@
+'use strict';
+const tap = require('tap');
+const Rapptor = require('rapptor');
+const fs = require('fs');
+const path = require('path');
+const wreck = require('wreck');
+
+let rapptor;
+tap.beforeEach((done) => {
+  rapptor = new Rapptor({ env: 'dev' });
+  rapptor.start((err, returned) => {
+    if (err) {
+      return done(err);
+    }
+    done();
+  });
+});
+
+tap.afterEach((done) => {
+  rapptor.stop(() => {
+    done();
+  });
+});
+
+tap.test('can POST to /upload', (assert) => {
+  const file = fs.readFileSync(path.join(__dirname, 'RandomBitmap.png'));
+  wreck.post('http://localhost:8080/upload', { payload: { file } }, (err, response) => {
+    assert.equal(err, null, 'does not error');
+    assert.end();
+  });
+});
