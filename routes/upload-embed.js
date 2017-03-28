b8a6847114c9fbf379b6a84c898c8e1e451f24e4
Eric Willis <eric@ecwillis.com>
2017-03-28 00:13:27 +0000

    Fixing validation bug from extra params

--- routes/upload-embed.js
+++ routes/upload-embed.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring');
+const pick = require('lodash.pick');
 exports.uploadMulti = {
   path: '/upload-multi',
   method: 'GET',
@@ -13,9 +14,11 @@ exports.uploadSingle = {
   path: '/upload-single',
   method: 'GET',
   handler(request, reply) {
+    const fullOptions = request.query;
+    const uploadOptions = pick(request.query, ['resize', 'width', 'height', 'background', 'quality', 'folder', 'public', 'url']);
     reply.view('upload-single', {
-      options: querystring.stringify(request.query),
-      opts: request.query
+      options: querystring.stringify(uploadOptions),
+      opts: fullOptions
     });
   }
 };
