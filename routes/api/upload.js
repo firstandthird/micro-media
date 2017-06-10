950dc75fbb954a94d717f5973fad124aa3c8c2b4
Chris <orthagonal1@gmail.com>
2017-06-10 13:18:11 -0500

    use wreck to allow http/https

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -45,24 +45,21 @@ exports.upload = {
           return done();
         }
         // fetch the file from url:
-        wreck.get(request.query.url, (response) => {
+        wreck.get(request.query.url, (err, response, payload) => {
+          if (err) {
+            return done(err);
+          }
           if (response.statusCode !== 200) {
             return done(boom.create(response.statusCode, `URL ${request.query.url} returned HTTP status code ${response.statusCode}`));
           }
           // abort if that file extension is not allowed:
           const ext = mime.extension(response.headers['content-type']);
           const filename = path.join(os.tmpdir(), `${Math.random()}.${ext}`);
-          const dataStream = new Stream();
-          response.on('data', (chunk) => {
-            dataStream.push(chunk);
-          });
-          response.on('end', () => {
-            fs.writeFile(filename, dataStream.read(), (err) => {
-              if (err) {
-                return done(err);
-              }
-              return done(null, filename);
-            });
+          fs.writeFile(filename, payload, (buffError) => {
+            if (buffError) {
+              return done(buffError);
+            }
+            return done(null, filename);
           });
         });
       },
