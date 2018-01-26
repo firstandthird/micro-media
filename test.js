c792602e4798787e8be5b61b2a4bfca2cc6716c6
orthagonal <orthagonal1@gmail.com>
2018-01-26 16:34:44 -0600

    thumb query option

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 const wreck = require('wreck');
 const f = async () => {
   try {
-   const { res, payload } = await wreck.post('http://localhost:8080/api/upload?url=http://vignette1.wikia.nocookie.net/sonnywithachance/images/d/d9/So_Random.png/revision/latest?cb=20110226213549');
+   const { res, payload } = await wreck.post('http://localhost:8080/api/upload?thumb=100x100&url=http://vignette1.wikia.nocookie.net/sonnywithachance/images/d/d9/So_Random.png/revision/latest?cb=20110226213549');
    console.log(Object.keys(res));
    console.log(res.statusCode);
    console.log(res.statusMessage);
