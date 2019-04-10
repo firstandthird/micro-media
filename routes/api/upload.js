f17baee1e550e5642931b4846d3a6df4217414bf
orthagonal <orthagonal1@gmail.com>
2019-04-10 05:15:37 +0000

    use jpegtran

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -1,7 +1,7 @@
 'use strict';
 const wreck = require('wreck');
 const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminSvgo = require('imagemin-svgo');
 const fs = require('fs');
@@ -104,12 +104,12 @@ exports.upload = {
     try {
       minBuffer = await imagemin.buffer(resizeBuffer, {
         plugins: [
-          imageminMozjpeg({ quality }),
           imageminPngquant({ quality }),
-          imageminSvgo({ quality })
+          imageminSvgo({ quality }),
+          imageminJpegtran({})
         ]
       });
-    } catch (err) {
+      } catch (err) {
       // anything other than error 99 is considered an unrecoverable error:
       if (err.code !== 99) {
         throw err;
